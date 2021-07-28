9b610464b08ea7951b10e428e63b89510c512c68
Muhammad Waqar <Muhammad.waqar@tiybr.com>
2021-07-28 10:20:08 +0400

    Add project files.

---
+++ BasicAuthentication/Handlers/BasicAuthenticationHandler.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.Handlers
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            :
+            base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            // skip authentication if endpoint has [AllowAnonymous] attribute
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+                return AuthenticateResult.NoResult();
+
+            // Check if Authorization param is available in Header
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+
+            try
+            {
+                var auth = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var bytes = Convert.FromBase64String(auth.Parameter);   //Ex:Mike:123
+                string[] credentials = Encoding.UTF8.GetString(bytes).Split(':');
+                var username = credentials[0];
+                var password = credentials[1];
+
+                if (username == "Mike" & password == "123")
+                {
+                    var claims = new[] {
+                        new Claim(ClaimTypes.Name, username)
+                        };
+                    var identity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                    return AuthenticateResult.Success(ticket);
+                }
+                else
+                {
+                    return AuthenticateResult.Fail("Invalid username or password");
+                }
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+        }
+    }
+}
